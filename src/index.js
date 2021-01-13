34ced080e365e12a5c71c53eb11da2114b916b19
Sabahet Alovic <sabaheta@yahoo.com>
2021-01-13 16:28:30 -0500

    first try

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import GifCard from './components/GifCard.js';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* //<App /> */}
+
+    <GifCard />
   </React.StrictMode>,
   document.getElementById('root')
 );
